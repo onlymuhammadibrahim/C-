5c420f6efa04c342db279312be16e12ff1678ec3
onlymuhammadibrahim <19-10666@formanite.fccollege.edu.pk>
2020-06-21 21:32:32 +0500

    Some Exercises

--- C Sharp/C-/Program.cs
+++ C Sharp/C-/Program.cs
@@ -10,6 +10,29 @@ namespace C_
     {
         static void Main(string[] args)
         {
+            /*//Time Converter
+            Console.Write("Enter the time in minutes: ");
+            int time = Convert.ToInt32(Console.ReadLine());
+            int hours = time / 60;
+            int minutes = time - (60 * hours);
+            Console.WriteLine("Total time is {0} Hours and {1} minutes", hours, minutes);
+            Console.ReadLine();*/
+            
+            
+            
+            
+            
+            /*// Cost of Carpeting
+            int num1, num2;
+            Console.Write("Enter First Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int costPerSquare = 50;
+            int area = num1 * num2;
+            int cost = costPerSquare * area; 
+            Console.WriteLine("The answer by adding two numbers is: " + cost);
+            Console.ReadLine();*/
         }
     }
 }
