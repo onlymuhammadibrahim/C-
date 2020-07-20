91b8953eeb0ff28762583e7cb79d620cff93227a
onlymuhammadibrahim <19-10666@formanite.fccollege.edu.pk>
2020-07-20 15:12:41 +0500

    Classes
    Excluding Inheritence

---
+++ C Sharp/C-/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        private int pages;
+
+        public Book(string aName, string aAuthor, int aPages)
+        {
+            name = aName;
+            author = aAuthor;
+            Pages = aPages;
+        }
+
+        public bool thick()
+        {
+            if (pages > 200)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int Pages
+        {
+            get { return pages; }
+            set { 
+                if ((value % 2) == 0)
+                {
+                    pages = value;
+                }
+                else
+                {
+                    pages = 999;
+                }
+            }
+        }
+    }
+}
--- C Sharp/C-/Program.cs
+++ C Sharp/C-/Program.cs
@@ -11,57 +11,15 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            string[] words = { "conference", "ham", "gallery", "essay", "signature", "internet", "corner", "gossip" };
+            Book book1 = new Book("Harry Potter", "jk rol", 401);
+            Book book2 = new Book("Sindbad", "IBM", 150);
 
-            Random randomGenerator = new Random();
-            int num = randomGenerator.Next(0, 8);
+            
 
-            string myString = words[num];
+            Console.WriteLine(book1.Pages);
 
-            string str = "*";
-            for (int i = 1; i < myString.Length; i++)
-            {
-                str += '*';
-            }
-
-            char guess;
-            Console.WriteLine(myString);
-            Console.WriteLine("Guess this word \n");
-            bool end = false;
-            string str1;
-            string str2;
-            while (!end)
-            {
-                Console.WriteLine(str);
-                Console.WriteLine("Enter the letter ");
-                guess = Convert.ToChar(Console.ReadLine());
-                if (myString.Contains(guess))
-                {
-                    Console.WriteLine("You Got that right");
-                    for(int j = 0; j < str.Length; j++)
-                    {
-                        if(myString[j] == guess)
-                        {
-                            str1 = str.Substring(0, j);
-                            str2 = str.Substring(j + 1);
-                            str = str1 + guess + str2;
-                            if (!(str.Contains('*')))
-                            {
-                                end = true;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Wrong Guess");
-                }
-                
-            }
-            Console.WriteLine("You Win!");
-
-
-
+            usefulTools.sayHi("IBM");
+            
             Console.ReadLine();
         }
     }
